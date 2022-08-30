d2038ac22f92c198d8b7d190aa4909aa7f731911
Andy Fernandez <andy.w.fernandez@gmail.com>
2022-08-29 18:15:09 -0600

    Added coverage stats

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 function medianScore(students) {
-  const sortedList = students.sort((a, b) => a.score - b.score);
+  if(!students.length) {return null;
+  } else {const sortedList = students.sort((a, b) => a.score - b.score);
   const midpoint = Math.floor(sortedList.length / 2);
 
-  return sortedList[midpoint].score;
+  return sortedList[midpoint].score};
 }
 
 function topScoringStudent(students) {
-  const sortedList = students.sort((a, b) => b.score - a.score);
-  return sortedList[0];
+  if(!students.length) {return null;
+  } else {const sortedList = students.sort((a, b) => b.score - a.score);
+  return sortedList[0]};
 }
 
 module.exports = { medianScore, topScoringStudent };
